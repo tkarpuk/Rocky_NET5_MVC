92d1e72accb5c1b1c5d085323877263529f1ed9b
Taras Karpuk <taras.karpuk@gmail.com>
2024-04-14 15:07:27 -0600

    Add DbContext

---
+++ Rocky/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rocky.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
