d93c777e81a7c1417da4a0ada935e28f591d695a
Taras Karpuk <taras.karpuk@gmail.com>
2024-04-19 16:22:45 -0600

    Add model ApplicationType

---
+++ Rocky/Models/ApplicationType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class ApplicationType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("App Type Name")]
+        public string Name { get; set; }
+    }
+}
