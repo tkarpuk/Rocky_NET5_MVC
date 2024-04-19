4b8f87997450ecf1793c963a5face3472b83cf02
Taras Karpuk <taras.karpuk@gmail.com>
2024-04-19 17:38:21 -0600

    Add ApplicationType controller + actions

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
+            return View(objList);
+        }
+
+        // GET Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }            
+        }
+
+        // GET Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // POST Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // POST Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+
+            _db.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
