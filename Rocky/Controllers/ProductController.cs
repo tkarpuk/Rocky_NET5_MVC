cfed935e8dde5e6e783557eba92365740d538377
Taras Karpuk <taras.karpuk@gmail.com>
2024-04-26 16:39:52 -0600

    Fix 2 small bags: empty image and empty Category list

--- Rocky/Controllers/ProductController.cs
+++ Rocky/Controllers/ProductController.cs
@@ -121,7 +121,14 @@ namespace Rocky.Controllers
             }
             else
             {
-                return View();
+                productVM.CategorySelectList = _db.Category.Select(c =>
+                    new SelectListItem()
+                    {
+                        Text = c.Name,
+                        Value = c.Id.ToString()
+                    });
+
+                return View(productVM);
             }
         }
 
