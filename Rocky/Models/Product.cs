f40c577e6139bafdb3bb4da0128eed72bf8df935
Taras Karpuk <taras.karpuk@gmail.com>
2024-04-27 14:18:53 -0600

    Add field ApplicationType into Product

--- Rocky/Models/Product.cs
+++ Rocky/Models/Product.cs
@@ -13,9 +13,15 @@ namespace Rocky.Models
         [Range(1, int.MaxValue)]
         public double Price { get; set; }
         public string Image { get; set; }
+
         [Display(Name="Category Type")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
+
+        [Display(Name = "Application Type")]
+        public int ApplicationTypeId { get; set; }
+        [ForeignKey("ApplicationTypeId")]
+        public ApplicationType ApplicationType { get; set; }
     }
 }
