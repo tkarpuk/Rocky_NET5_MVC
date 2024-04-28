4dd678befb98ba158ebf6d0d2dc977f3eccbb1ec
Taras Karpuk <taras.karpuk@gmail.com>
2024-04-28 14:50:14 -0600

    Add HomeVM

---
+++ Rocky/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Rocky.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
