a7f469ba049dfd2ae6714951cfd26f9d969e5259
Taras Karpuk <taras.karpuk@gmail.com>
2024-04-29 22:19:12 -0600

    Add entry to ShoppingCart

--- Rocky/GeneralConstant.cs
+++ Rocky/GeneralConstant.cs
@@ -3,5 +3,6 @@
     public static class GeneralConstant
     {
         public static string ImagePath = @"\images\product\";
+        public static string SessionCart = "ShoppingCartSession";
     }
 }
