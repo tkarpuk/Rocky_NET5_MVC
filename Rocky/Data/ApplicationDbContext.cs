462c458e1862eb551a78edfbabedb30b9a695ea8
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-04 14:53:25 -0600

    Add ApplicationUser model

--- Rocky/Data/ApplicationDbContext.cs
+++ Rocky/Data/ApplicationDbContext.cs
@@ -13,5 +13,6 @@ namespace Rocky.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<ApplicationType> ApplicationType { get; set; }
         public DbSet<Product> Product { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
     }
 }
