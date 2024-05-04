2100c9bb7376d11524ef6d1e06802859172fe129
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-04 14:36:36 -0600

    Add RazorPages for Identity

--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -64,6 +64,7 @@ namespace Rocky
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
