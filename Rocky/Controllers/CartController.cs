c961f7a362ec5923196785994ef5c0e993e8907c
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-05 16:05:03 -0600

    Add actions into CartController

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 using Rocky.Utility;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace Rocky.Controllers
 {
@@ -13,6 +15,9 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext db)
         {
@@ -29,6 +34,33 @@ namespace Rocky.Controllers
             List<Product> prodList = _db.Product.Where(p => ids.Contains(p.Id)).ToList();
 
             return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.Claims.FirstOrDefault();
+
+            List<ShoppingCart> cart = HttpContext.Session.Get<List<ShoppingCart>>(GeneralConstant.SessionCart)
+                ?? new List<ShoppingCart>();
+            List<int> ids = cart.Select(i => i.ProductId).ToList();
+            List<Product> prodList = _db.Product.Where(p => ids.Contains(p.Id)).ToList();
+
+            ProductUserVM productUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = prodList
+            };
+
+            return View(productUserVM);
         }
 
         public IActionResult Remove(int id)
