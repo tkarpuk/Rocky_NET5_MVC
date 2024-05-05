f4b68b34b584522a776b1cbab1d0661dcd6d92c4
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-05 16:04:18 -0600

    Add ProductUserVM

---
+++ Rocky/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Rocky.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public List<Product> ProductList { get; set; } = new List<Product>();
+    }
+}
