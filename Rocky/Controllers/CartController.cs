33b76f2497c4653865064a1d4bcd1ff170935c23
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-05 14:36:39 -0600

    Add CartController

---
+++ Rocky/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Utility;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rocky.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> cart = HttpContext.Session.Get<List<ShoppingCart>>(GeneralConstant.SessionCart) 
+                ?? new List<ShoppingCart>();
+
+            List<int> ids = cart.Select(i => i.ProductId).ToList();
+
+            List<Product> prodList = _db.Product.Where(p => ids.Contains(p.Id)).ToList();
+
+            return View(prodList);
+        }
+    }
+}
