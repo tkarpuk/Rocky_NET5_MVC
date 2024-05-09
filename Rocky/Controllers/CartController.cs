b56f1720aa5be1ef0c5dce909edc8e7852be4473
Taras Karpuk <taras.karpuk@gmail.com>
2024-05-09 15:58:36 -0600

    Send InquireConfirmation

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
 using Rocky.Utility;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Rocky.Controllers
 {
@@ -15,13 +21,17 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IEmailSender _emailSender;
 
         [BindProperty]
         public ProductUserVM ProductUserVM { get; set; }
 
-        public CartController(ApplicationDbContext db)
+        public CartController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment, IEmailSender emailSender)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
+            _emailSender = emailSender;
         }
 
         public IActionResult Index()
@@ -62,6 +72,48 @@ namespace Rocky.Controllers
 
             return View(productUserVM);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Summary")]
+        public async Task<IActionResult> SummaryPost(ProductUserVM productUserVM) 
+        { 
+            //string pathToTemplate = _hostEnvironment.ContentRootPath + Path.DirectorySeparatorChar.ToString()
+            //    + "templates" + Path.DirectorySeparatorChar.ToString()
+            //    + "Inquiry.html";
+
+            var pathTemplate = $"{_webHostEnvironment.WebRootPath}\\templates\\Inquiry.html";
+            string subject = "New inquery";
+            string htmlBody = string.Empty;
+            using (StreamReader sr = new StreamReader(pathTemplate))
+            {
+                htmlBody = sr.ReadToEnd();
+            }
+
+            StringBuilder productListSB = new StringBuilder();
+            foreach (var product in productUserVM.ProductList)
+            {
+                productListSB.Append(
+                    $" - Name: {product.Name} <span style='font-size:14px;'>(ID: {product.Id})</span><br />");
+            }
+
+            string messageBody = string.Format(htmlBody,
+                productUserVM.ApplicationUser.FullName,
+                productUserVM.ApplicationUser.Email,
+                productUserVM.ApplicationUser.PhoneNumber,
+                productListSB.ToString());
+
+            await _emailSender.SendEmailAsync(GeneralConstant.EmailAdmin, subject, messageBody);
+
+            return RedirectToAction(nameof(InquireConfirmation)); 
+        }
+
+        public IActionResult InquireConfirmation()
+        {
+            HttpContext.Session.Clear();
+            return View();
+        }
+
 
         public IActionResult Remove(int id)
         {
